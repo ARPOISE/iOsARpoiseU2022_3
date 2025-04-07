441d62c042cf734ef7340c2fc66865a98aa4d5da
Peter Graf <Peter.Graf@aquantec.de>
2025-04-07 22:54:07 +0200

    Version 2.0.12, 25040300

--- Assets/Scripts/ArBehaviourMultiUser.cs
+++ Assets/Scripts/ArBehaviourMultiUser.cs
@@ -50,7 +50,7 @@ namespace com.arpoise.arpoiseapp
 #else
         public const string OperatingSystem = "Android";
 #endif
-        public const int Bundle = 25040100;
+        public const int Bundle = 25040300;
         public const string ArvosApplicationName = "Arvos";
         public const string ArpoiseApplicationName = "Arpoise";
 #if AndroidArvosU2022_3 || iOsArvosU2022_3
