47ee1a9a47077e0cd9682ffd16b1f608853589a5
Peter Graf <Peter.Graf@aquantec.de>
2025-10-06 21:25:00 +0200

    Switch to Unity 2022.3.62

--- Assets/Scripts/ArBehaviourMultiUser.cs
+++ Assets/Scripts/ArBehaviourMultiUser.cs
@@ -50,7 +50,7 @@ namespace com.arpoise.arpoiseapp
 #else
         public const string OperatingSystem = "Android";
 #endif
-        public const int Bundle = 25091500;
+        public const int Bundle = 25100600;
         public const string ArvosApplicationName = "Arvos";
         public const string ArpoiseApplicationName = "Arpoise";
 #if AndroidArvosU2022_3 || iOsArvosU2022_3
